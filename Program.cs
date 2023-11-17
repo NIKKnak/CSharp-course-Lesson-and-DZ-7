ccdce8b64498df536fc31467c2944fc53852c1ce
NIKKnak <nik88350759@yandex.ru>
2023-11-17 21:14:39 +0300

    DZ-7 end

--- Program.cs
+++ Program.cs
@@ -2,60 +2,34 @@
 using System.Text.RegularExpressions;
 using System.Collections.Specialized;
 using System.Text;
+using System.Reflection;
 
 namespace CSharp_course_Lesson_and_DZ_7
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
 
-            //создать методы создающий этот класс вызывая один из его 
-            //конструкторов(по одному конструктору на метод). Задача не очень 
-            //сложна и служит больше для разогрева перед следующей задачей.
-
             var v = CreateTestClassInstance(1, 11, "ssad", 3, new char[] { '4', '3', '8', '2' }, new char[] { 'f', 'g', 'k', 'i' });
-            /*
-            Напишите 2 метода использующие рефлексию
-            1 - сохраняет информацию о классе в строку
-            2 - позволяет восстановить класс из строки с информацией о методе
-            В качестве примере класса используйте класс TestClass.
-            Шаблоны методов для реализации:
-            static object StringToObject(string s) { }
-            static string ObjectToString(object o) { }
-            */
 
-            /*
-            Пример того как мог быть выглядеть сохраненный в строку объект:
-            “TestClass, test2, Version = 1.0.0.0, Culture = neutral, PublicKeyToken = null:TestClass | I:1 | S:STR | D:2.0 |”
-            */
             string str = ObjectToString(v);
             Console.WriteLine(str);
             object str2 = StringToObject(str);
+
         }
 
-        public static TestClass CreateTestClassInstance(int i,int ii, string s, decimal d, char[] c, char[] cc)
+        public static TestClass CreateTestClassInstance(int i, int ii, string s, decimal d, char[] c, char[] cc)
         {
             var testClassType = typeof(TestClass);
             var testClass = Activator.CreateInstance(testClassType) as TestClass;
 
             var testClassTwo = Activator.CreateInstance(testClassType, new object[] { i });
 
-            var testClassThird = Activator.CreateInstance(testClassType, new object[] { i,ii, s, d, c,cc });
+            var testClassThird = Activator.CreateInstance(testClassType, new object[] { i, ii, s, d, c, cc });
 
             return testClassThird as TestClass;
         }
-        // "CSharp-course-Lesson-and-DZ-7, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"
-        public static object StringToObject(string endString)
-        {
-            string[] str = endString.Split("\n");
-            var obj = Activator.CreateInstance(str[1], str[0])?.Unwrap();
-
-
-
-            return endString;
-        }
-        // “TestClass, test2, Version = 1.0.0.0, Culture = neutral, PublicKeyToken = null:TestClass | I:1 | S:STR | D:2.0 |”
         public static string ObjectToString(object obj)
         {
             StringBuilder stringBuilder = new StringBuilder();
@@ -68,18 +42,64 @@ namespace CSharp_course_Lesson_and_DZ_7
             {
                 var value = item.GetValue(obj);
 
-                stringBuilder.Append(item.Name + ":");
+                stringBuilder.Append(GetPropertyName(item) + ":");
                 if (item.PropertyType == typeof(char[]))
                 {
-                    stringBuilder.Append(new String(value as char[]) +"\n");
+                    stringBuilder.Append(new String(value as char[]) + "\n");
                 }
                 else
                 {
                     stringBuilder.Append(value + "\n");
                 }
             }
-            
+
             return stringBuilder.ToString();
+        }
+
+
+        private static string GetPropertyName(PropertyInfo property)
+        {
+            var customNameAttribute = (CustomNameAttribute)Attribute.GetCustomAttribute(property, typeof(CustomNameAttribute));
+            return customNameAttribute != null ? customNameAttribute.CustomFieldName : property.Name;
+        }
+
+
+        public static object StringToObject(string endString)
+        {
+            string[] str = endString.Split("\n");
+            var typeName = str[2];
+            var assemblyName = str[1];
+            var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName == assemblyName);
+            if (assembly != null)
+            {
+                var type = assembly.GetTypes().FirstOrDefault(t => t.FullName == typeName);
+                if (type != null)
+                {
+                    var obj = Activator.CreateInstance(type);
+                    var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                    for (int i = 3; i < str.Length - 1; i++)
+                    {
+                        var propertyString = str[i].Split(":");
+                        var propertyName = propertyString[0];
+                        var propertyValue = propertyString[1];
+                        var property = properties.FirstOrDefault(p => GetPropertyName(p) == propertyName.Trim());  // Используем вспомогательный метод для получения имени поля
+                        if (property != null)
+                        {
+                            if (property.PropertyType == typeof(int))
+                            {
+                                property.SetValue(obj, int.Parse(propertyValue.Trim()));
+                            }
+                            else if (property.PropertyType == typeof(string))
+                            {
+                                property.SetValue(obj, propertyValue.Trim());
+                            }
+                            
+                        }
+                    }
+                    return obj;
+                }
+            }
+            return null;
         }
 
 
--- TestClass.cs
+++ TestClass.cs
@@ -6,24 +6,41 @@ using System.Threading.Tasks;
 
 namespace CSharp_course_Lesson_and_DZ_7
 {
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class CustomNameAttribute : Attribute
+    {
+        public string CustomFieldName { get; }
+
+        public CustomNameAttribute(string customFieldName)
+        {
+            CustomFieldName = customFieldName;
+        }
+    }
+
+
+
     internal class TestClass
     {
-        public int I {  get; set; }
-        private int II {  get; set; }
+        [CustomName("CustomFieldName")]
+        public int I { get; set; }
+        [CustomName("CustomFieldName")]
+        public int II { get; set; }
+        [CustomName("CustomFieldName")]
         public string? S { get; set; }
+        [CustomName("CustomFieldName")]
         public decimal? D { get; set; }
         public char[]? C { get; set; }
-        private char[]? CC { get; set; } 
+        private char[]? CC { get; set; }
 
         public TestClass()
         {
-            
+
         }
         public TestClass(int i)
         {
             I = i;
         }
-        public TestClass(int i,int ii,string s, decimal d, char[] c, char[] cc) : this(i)
+        public TestClass(int i, int ii, string s, decimal d, char[] c, char[] cc) : this(i)
         {
             S = s;
             D = d;
